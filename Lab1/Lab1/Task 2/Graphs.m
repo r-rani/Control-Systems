c27d6fc26e7a1bbce097e3b68084ee5dc0668c05
Richa Rani <rani.richa2002@gmail.com>
2024-10-02 14:51:50 -0400

    made changes to graph file

--- Lab1/Lab1/Task 2/Graphs.m
+++ Lab1/Lab1/Task 2/Graphs.m
@@ -1,17 +1,49 @@
 
 x = out.Lab1_Task3;
-plot(x(:,1),x(:,2)); %graph for lab 1 task 3
-xlabel('Time'), ylabel('Position')
-sgtitle('Time vs Position');
+y = out.Lab1_Task;
+z = out.Lab1_Task7;
+
+
+plot(z(:,1),z(:,2),'y'); %graph for lab 1 task 6 and 7
+hold on
+plot(z(:,1),z(:,3),'b'); %graph for lab 1 task 7
+hold on
+plot(z(:,1),z(:,4),'r'); %graph for lab 1 task 7
+hold on
+plot(z(:,1),z(:,5),'g'); %graph for lab 1 task 7
+hold off
+xlabel('Time'), ylabel('Position/Velocity')
+title('Time vs Position/Velocity');
+set(gca,'color',[0.5 0.5 0.5])
+
 
 %{
-y = out.Lab1_Task4;
-
-plot(y(:,1),y(:,2));
+subplot(2,1,1);
+plot(x(:,1),x(:,2)); %graph for lab 1 task 3
 xlabel('Time'), ylabel('Position')
-sgtitle('Time vs Position (C = 0)');
+title('Time vs Position (With Approximation at Pi)');
 
+subplot(2,1,2);
+plot(x(:,1),x(:,3)); %graph for lab 1 task 3
+xlabel('Time'), ylabel('Velocity')
+title('Time vs Velocity (With Approximation at Pi)');
 %}
+
+%{
+subplot(2,1,1);
+plot(y(:,1),y(:,2)); %graph for lab 1 task 3
+xlabel('Time'), ylabel('Position')
+title('Time vs Position (Without Approximation at Pi)');
+ylim([3.1 3.2]);
+
+subplot(2,1,2);
+plot(y(:,1),y(:,3)); %graph for lab 1 task 3
+xlabel('Time'), ylabel('Velocity')
+title('Time vs Velocity (Without Approximation at Pi)');
+ylim([-0.1 0.1]);
+%}
+
+
 
 
 
